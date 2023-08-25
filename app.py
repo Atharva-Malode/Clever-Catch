253758a5eed5c19081d1b1b1373c836b7f74c681
unknown <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-08-25 11:46:18 +0530

    Update: Removed the index.css file mounting

---
+++ app.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from extractor import ExtractFeatures
+import pandas as pd
+import pickle
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Load the trained model using pickle
+with open('xgb.pkl', 'rb') as file:
+    loaded_tuned_model = pickle.load(file)
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/submit", response_class=HTMLResponse)
+async def submit_form(request: Request):
+    form_data = await request.form()
+    extractor = ExtractFeatures()
+    url = form_data["url"]
+    features = extractor.url_to_features(url)
+    dataframe = pd.DataFrame([features])
+    predictions = loaded_tuned_model.predict(dataframe)
+    print(predictions)
+    if predictions[0] == 1:
+        return templates.TemplateResponse("result.html", {"request": request, "url": form_data["url"], "prediction": "Not Phishing"})
+    return templates.TemplateResponse("result.html", {"request": request, "url": form_data["url"], "prediction": "Phishing"})
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="8000")
